51fb1b80dd4399132c4e4e88480cff3b99ba4436
Miguel Camba <miguel.camba@gmail.com>
2020-01-16 00:47:20 +0100

    Export 5 helpers

---
+++ src/index.js
@@ -0,0 +1,19 @@
+export function __plural(value, opts) {
+  return `pluralized ${value}!`;
+}
+
+export function __select(value, options) {
+  return `selected ${value}!`;
+}
+
+export function __number(value, style) {
+  return `numbered ${value}!`;
+}
+
+export function __date(value, style) {
+  return `dated ${value}!`;
+}
+
+export function __time(value, style) {
+  return `timed ${value}!`;
+}
