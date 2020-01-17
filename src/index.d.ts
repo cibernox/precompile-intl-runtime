5825d68342ed84f9f31676238d68a3ce3fb66506
Miguel Camba <miguel.camba@gmail.com>
2020-01-17 15:24:16 +0100

    Add types por new objects

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,14 @@
 declare module "icu-helpers" {
   type MessageFn = (...args: any) => string
+  interface WritableStore {
+    subscribe: (fn: () => {}) => () => {}
+    set(v: any): void
+    update(cb: () => {}): void
+    clear(): void
+  }
+  export const currentLocale: WritableStore
+  export const locales: WritableStore
+  export const dictionary: WritableStore
   declare function addMessages(locale: any, messages: any): void
   declare function setLocale(locale: string): void
   declare function lookupMessage(key: string, locale?: string): string | MessageFn
