523883cb057067a6775835c729597c97343c7551
Miguel Camba <miguel.camba@gmail.com>
2020-01-17 17:24:22 +0100

    Document exported init function

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,4 +12,5 @@ declare module "icu-helpers" {
   declare function addMessages(locale: any, messages: any): void
   declare function setLocale(locale: string): void
   declare function lookupMessage(key: string, locale?: string): string | MessageFn
+  declare function init(Record): void
 }
