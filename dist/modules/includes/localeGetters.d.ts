31d74a76c900d225ee99bc8b1bb9ad1aba799882
Miguel Camba <miguel.camba@gmail.com>
2022-09-30 20:12:40 +0200

    Update dependencies

--- dist/modules/includes/localeGetters.d.ts
+++ dist/modules/includes/localeGetters.d.ts
@@ -1,6 +1,6 @@
 export declare const getLocaleFromHostname: (hostname: RegExp) => string | null;
 export declare const getLocaleFromPathname: (pathname: RegExp) => string | null;
-export declare const getLocaleFromNavigator: (ssrDefault?: string | undefined) => string | null;
+export declare const getLocaleFromNavigator: (ssrDefault?: string) => string | null;
 export declare const getLocaleFromQueryString: (search: string) => string | null;
 export declare const getLocaleFromHash: (hash: string) => string | null;
-export declare const getLocaleFromAcceptLanguageHeader: (header: string | null, availableLocales?: string[] | undefined) => string | undefined;
+export declare const getLocaleFromAcceptLanguageHeader: (header: string | null, availableLocales?: string[]) => string | undefined;
