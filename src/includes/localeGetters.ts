1566990132cad76fdcb22398be2e223faadfc01f
Marco Selenati <marcoselenati@gmail.com>
2022-12-19 13:07:54 +0700

    Remove seo unfriendly localeGetters

--- src/includes/localeGetters.ts
+++ src/includes/localeGetters.ts
@@ -1,13 +1,3 @@
-const getFromQueryString = (queryString: string, key: string) => {
-  const keyVal = queryString.split("&").find((i) => i.indexOf(`${key}=`) === 0);
-
-  if (keyVal) {
-    return keyVal.split("=").pop() || null;
-  }
-
-  return null;
-};
-
 const getFirstMatch = (base: string, pattern: RegExp) => {
   const match = pattern.exec(base);
 
@@ -39,20 +29,6 @@ export const getLocaleFromNavigator = (ssrDefault?: string) => {
   }
 
   return window.navigator.language || window.navigator.languages[0];
-};
-
-export const getLocaleFromQueryString = (search: string) => {
-  // istanbul ignore next
-  if (typeof window === "undefined") return null;
-
-  return getFromQueryString(window.location.search.substr(1), search);
-};
-
-export const getLocaleFromHash = (hash: string) => {
-  // istanbul ignore next
-  if (typeof window === "undefined") return null;
-
-  return getFromQueryString(window.location.hash.substr(1), hash);
 };
 
 export const getLocaleFromAcceptLanguageHeader = (
